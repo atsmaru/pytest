cb4a201b0388aa67b3b92ccd1e543161639ac12c
atsmaru <atsmaru@gmail.com>
2021-04-03 23:03:34 +0900

    add test_app.py

---
+++ test_app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import pytest
+
+# 「テスト変数」
+@pytest.fixture
+def variable():
+    return "hi"
+
+
+# "test" という文字が関数にあれば、pytest はそれをテストケースとして認識します。
+def test_1():
+    a = 2
+    b = 2
+    assert a == b
+
+def test_2():
+    a = 1
+    b = 2
+    assert a != b
+
+
+def test_3(variable):
+    assert variable == "hi"
